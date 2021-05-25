c7ba2c03de302fcc498e207eabfe7159835bbfa3
mrcs22 <marcusmoraes2010@hotmail.com>
2021-05-25 11:58:21 -0300

    Starting Timeline

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Timeline from "./components/timeline/Timeline";
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+   font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    
+  }`;
 
-export default function App(){
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact>
-
-                </Route>
-                <Route path="/sign-up" exact>
-
-                </Route>
-                <Route path="/timeline" exact>
-
-                </Route>
-                <Route path="/my-posts" exact>
-
-                </Route>
-                <Route path="/my-likes" exact>
-
-                </Route>
-                <Route path="/hashtags/:hashtag" exact>
-
-                </Route>
-                <Route path="/user/:id" exact>
-
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact></Route>
+        <Route path="/sign-up" exact></Route>
+        <Route path="/timeline" exact component={Timeline}></Route>
+        <Route path="/my-posts" exact></Route>
+        <Route path="/my-likes" exact></Route>
+        <Route path="/hashtags/:hashtag" exact></Route>
+        <Route path="/user/:id" exact></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+  min-height: 100vh;
+  width: 100%;
+
+  padding-top: 72px;
+
+  background-color: #333;
+`;
