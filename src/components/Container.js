0170618a691e765395531b3337a7fc1824d4298c
mrcs22 <marcusmoraes2010@hotmail.com>
2021-05-25 16:21:30 -0300

    Finishing Container structure

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,25 @@
 import styled from "styled-components";
 
-export default styled.div`
+export default function Container({ children }) {
+  return (
+    <Div>
+      <Content>{children}</Content>
+    </Div>
+  );
+}
+
+const Div = styled.div`
   min-height: 100vh;
   width: 100%;
 
   padding-top: 72px;
 
   background-color: #333;
+`;
+
+const Content = styled.div`
+  width: 937px;
+
+  margin: 0 auto;
+  margin-top: 53px;
 `;
