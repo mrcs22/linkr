ac0e08752aa8cbfa0c8740a2ba2af9f321182269
jotabraga <jpmbraga.feg@gmail.com>
2021-05-27 01:42:25 -0300

    Hashtag page implementation

--- src/components/hashtag/Hashtag.js
+++ src/components/hashtag/Hashtag.js
@@ -1,28 +1,79 @@
-import { useParams } from 'react-router-dom';
-import {useState, useEffect, useContext} from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import UserContext from "../UserContext";
+import styled from "styled-components";
 
-export default function Hashtag(){
+import Container from "../Container";
+import Header from "../header/Header";
 
-    const { hashtag } = useParams();
-    const [hash, setHash] = useState([]);
-    const { user } = useContext(UserContext);
+import Post from "../post/Post";
+import PuffLoader from "../Loader";
 
-    useEffect(() => {
+export default function Hashtag() {
+  const { hashtag } = useParams();
+  const [tags, setTags] = useState([]);
+  const { user } = useContext(UserContext);
 
-        const config = {headers:{Authorization: `Bearer ${user.token}`} }   
-        
-        const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/${hashtag}/posts`, config);
+  useEffect(() => {
+    const config = { headers: { Authorization: `Bearer ${user.token}` } };
 
-        promise.then((answer) => {
-            setHash(answer.data);    
-        });
-    }, [hashtag, user]);
-
-    return(
-
-        <div></div>
-
+    const promise = axios.get(
+      `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/${hashtag}/posts`,
+      config
     );
+
+    promise.then((answer) => {
+      setTags(answer.data);
+    });
+  }, [hashtag, user]);
+
+  return (
+    <>
+      <Header avatar={user.user.avatar} />
+
+      <Container>
+        <Text>#{Hashtag}</Text>
+
+        {tags === null ? (
+          <PuffLoader />
+        ) : tags.length === 0 ? (
+          <Text noPosts>Nenhum Post encontrado</Text>
+        ) : (
+          tags.map((p) => (
+            <Post
+              key={p.id}
+              postId={p.id}
+              username={p.user.username}
+              userId={p.user.id}
+              avatar={p.user.avatar}
+              text={p.text}
+              link={p.link}
+              linkTitle={p.linkTitle}
+              linkDescription={p.linkDescription}
+              linkImage={p.linkImage}
+            />
+          ))
+        )}
+      </Container>
+    </>
+  );
 }
+
+const Text = styled.p`
+  font-family: "Oswald";
+
+  font-size: ${({ noPosts }) => (noPosts ? "30px" : "43px")};
+  color: ${({ noPosts }) => (noPosts ? "#bababa" : "#fff")};
+
+  margin-left: ${({ noPosts }) => (noPosts ? "130px" : 0)};
+  margin-top: ${({ noPosts }) => (noPosts ? "20px" : 0)};
+  margin-bottom: 45px;
+
+  @media (max-width: 611px) {
+    font-size: 33px;
+
+    margin-bottom: 29px;
+    margin-left: 17px;
+  }
+`;
