3c90f66c4e0023f7301397cc6086401aa37b0f0d
jotabraga <jpmbraga.feg@gmail.com>
2021-06-01 01:09:18 -0300

    Search bar hashtag improvement

--- src/components/hashtag/HashtagTrend.js
+++ src/components/hashtag/HashtagTrend.js
@@ -2,11 +2,13 @@ import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import UserContext from "../UserContext";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function HashtagTrend() {
   const [trend, setTrend] = useState([]);
   const { user } = useContext(UserContext);
+  const [keyword, setKeyword] = useState("");
+  let history = useHistory();
 
   useEffect(() => {
     const config = { headers: { Authorization: `Bearer ${user.token}` } };
@@ -21,6 +23,11 @@ export default function HashtagTrend() {
     });
   }, [user]);
 
+  function search(e) {
+    e.preventDefault();
+    history.push(`/hashtag/${keyword}`);
+  }
+
   return (
     <TrendBox>
       <Title>trending</Title>
@@ -30,18 +37,28 @@ export default function HashtagTrend() {
             <h1>#{item.name}</h1>
           </Link>
         ))}
+        <HashtagSearch>
+        <form onSubmit={search}>
+          <input
+            type="text"
+            placeholder="# type a hashtag"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}>
+          </input>          
+        </form>
+      </HashtagSearch>
       </Trends>
+
     </TrendBox>
   );
 }
 
 const TrendBox = styled.div`
   width: 300px;
-  height: 400px;
+  height: 410px;
   border-radius: 16px;
   background: #171717;
   color: #fff;
-
   font-weight: 700;
   position: fixed;
   left: calc(50vw + 150px);
@@ -65,14 +82,36 @@ const Title = styled.div`
 const Trends = styled.div`
   font-family: "Lato";
   max-width: 100%;
-  height: 293px;
-  overflow: hidden;
+  height: 293px;  
   padding-left: 16px;
-  margin-top: 22px;
+  padding-right: 16px;
+  margin-top: 15px;
 
   h1 {
     font-size: 20px;
     line-height: 28px;
     letter-spacing: 0.05em;
   }
+`;
+const HashtagSearch = styled.div`
+  font-family: "Lato";
+  p{
+    font-weight: 700;
+    font-size:19px;
+    line-height: 23px;
+    color: #fff;
+    text-align: left;          
+  }
+  input{
+    width: 100%;
+    border: none;
+    height: 35px;
+    padding-left: 13px;    
+    margin-right: 16px;
+    margin-top: 14px;
+    border-radius: 8px;
+    background: #252525;
+    color: #575757;     
+    font-size: 16px;
+  }  
 `;
