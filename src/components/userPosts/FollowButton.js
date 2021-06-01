a525ee42a266effb2072a27ce64598bfd2c4faf8
Daniel Capeans <dcapeans@gmail.com>
2021-06-01 12:46:08 -0300

    created follow button

---
+++ src/components/userPosts/FollowButton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export default function FollowButton(){
+    return(
+        <StyledButton />
+    )
+}
+
+const StyledButton = styled.button`
+    width: 112px;
+    height: 31px;
+    background-color: #1877f2;
+    color: #fff;
+`
