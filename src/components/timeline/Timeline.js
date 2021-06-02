01d5c9f4e07b2cbad254505c69523668ac6ed88b
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-02 16:27:18 -0300

    Adding infinte-scroll component and refactoring getPosts function

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
+import InfiniteScroll from "react-infinite-scroller";
 import useInterval from "../../helpers/useInterval";
 
 import Container from "../Container";
@@ -14,31 +15,42 @@ import HashtagTrend from "../hashtag/HashtagTrend";
 export default function Timeline() {
   const { user } = useContext(UserContext);
   const [posts, setPosts] = useState(null);
-  const [likedUsers, setLikedUsers] = useState(null)
+  const [likedUsers, setLikedUsers] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+  const olderLoadedPostId = posts === null ? null : posts[posts.length - 1].id;
 
   useEffect(() => {
     getPosts(user.token);
-    getLikedUsers(user.token)
+    getLikedUsers(user.token);
   }, [user.token]);
 
   useInterval(() => {
     getPosts(user.token);
   }, 15000);
 
-  
-  function getPosts(token) {
+  function getPosts(token, older) {
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     };
-    const req = axios.get(
-      "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/following/posts",
-      config
-    );
+
+    const url = `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/following/posts${
+      older ? `?olderThan=${older}` : ""
+    }`;
+
+    const req = axios.get(url, config);
 
     req.then((r) => {
-      setPosts(r.data.posts);
+      if (older) {
+        if (r.data.posts.length === 0) {
+          setHasMore(false);
+          return;
+        }
+        setPosts([...posts, ...r.data.posts]);
+      } else {
+        checkForPostsUpdate(r.data.posts);
+      }
     });
 
     req.catch((r) => {
@@ -46,20 +58,47 @@ export default function Timeline() {
     });
   }
 
-const getLikedUsers = (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  function checkForPostsUpdate(receivedPosts) {
+    if (posts === null) {
+      setPosts(receivedPosts);
+    } else {
+      const newPosts = [];
+
+      receivedPosts.forEach((rp) => {
+        let doPush = true;
+
+        posts.forEach((p) => {
+          if (p.id === rp.id) {
+            doPush = false;
+          }
+        });
+
+        if (doPush) {
+          newPosts.push(rp);
+        }
+      });
+
+      setPosts([...newPosts, ...posts]);
+    }
+  }
+
+  const getLikedUsers = (token) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const request = axios.get(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/follows",
+      config
+    );
+    request.then((res) => {
+      setLikedUsers(res.data.users);
+    });
+    request.catch((err) => {
+      alert("Houve uma falha ao obter os posts, por favor atualize a página!");
+    });
   };
-  const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/follows", config)
-  request.then((res) => {
-    setLikedUsers(res.data.users)
-  })
-  request.catch((err) => {
-    alert("Houve uma falha ao obter os posts, por favor atualize a página!");
-  })
-}
 
   return (
     <>
@@ -75,22 +114,29 @@ const getLikedUsers = (token) => {
         ) : posts.length === 0 ? (
           <Text noPosts>Nenhuma publicação encontrada</Text>
         ) : (
-          posts.map((p) => (
-            <Post
-              key={p.id}
-              postId={p.id}
-              username={p.user.username}
-              userId={p.user.id}
-              avatar={p.user.avatar}
-              text={p.text}
-              link={p.link}
-              linkTitle={p.linkTitle}
-              linkDescription={p.linkDescription}
-              linkImage={p.linkImage}
-              getPosts={getPosts}
-              likes={p.likes}
-            />
-          ))
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={() => getPosts(user.token, olderLoadedPostId)}
+            hasMore={hasMore}
+            loader={<PuffLoader />}
+          >
+            {posts.map((p) => (
+              <Post
+                key={p.id}
+                postId={p.id}
+                username={p.user.username}
+                userId={p.user.id}
+                avatar={p.user.avatar}
+                text={p.text}
+                link={p.link}
+                linkTitle={p.linkTitle}
+                linkDescription={p.linkDescription}
+                linkImage={p.linkImage}
+                getPosts={getPosts}
+                likes={p.likes}
+              />
+            ))}
+          </InfiniteScroll>
         )}
       </Container>
     </>
