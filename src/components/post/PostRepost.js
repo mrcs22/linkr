2f1b061baf277952e27152c9fad89dd0d3c2ef2b
jotabraga <jpmbraga.feg@gmail.com>
2021-06-04 04:32:59 -0300

    Add of repost component

--- src/components/post/PostRepost.js
+++ src/components/post/PostRepost.js
@@ -1,11 +1,19 @@
-
+import { BiRepost } from "react-icons/bi";
 
 export default function PostRepost(pros) {
 
-    return(
-        <div>
-            
-        </div>
+    const { comments, visibility, setVisibility, getComments } = props;
+
+
+    function openCommentbox(){
+      getComments();
+      setVisibility(!visibility);
+    }
+  
+    return (
+      <div>
+          <BiRepost onClick={toRepost} />
+          <p className="likes">{reposts} re-posts</p> 
+      </div>
     );
-    
-}
+  }
